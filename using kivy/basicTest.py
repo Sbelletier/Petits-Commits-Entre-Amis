5c19c80cd9530c91c8253c8cfb69b5d2857a2980
Simon Belletier <simon.belletier@etu.univ-amu.fr>
2016-08-02 20:36:55 +0200

    on_touch_down
    Gestion de clic

--- using kivy/basicTest.py
+++ using kivy/basicTest.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout#How a widget is created
+#A type of layout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
@@ -20,13 +21,29 @@ class LoginScreen(GridLayout):
         self.password = TextInput(password=True, multiline=False,size_hint=[0.2,1.0])
         self.add_widget(self.password)
 
+class ComputingButton(Button):
+	def __init__(self, **kwargs):
+		super(ComputingButton, self).__init__(**kwargs)
+	def on_touch_down(self, touch):
+		#Catches event outside its range, so you have to make sure there is collision
+		if self.collide_point(*touch.pos):
+			print "that rustled my computing button"
+			return True
+		print "what was that ?"
+		return super(ComputingButton, self).on_touch_down(touch)
+		
 class BigWidget(GridLayout):
 	def __init__(self, **kwargs):
 		super(BigWidget, self).__init__(**kwargs)
 		self.rows = 3
 		self.add_widget(LoginScreen())
-		self.add_widget(Button(text='Compute'))
-		
+		self.add_widget(ComputingButton(text='Compute'))
+	def on_touch_down(self, touch):
+		print "I felt something"
+		#Even though it is counter intuitive, this dispatches
+		#The event to children
+		return super(BigWidget, self).on_touch_down(touch)
+	
 		
 class MyApp(App):
 
