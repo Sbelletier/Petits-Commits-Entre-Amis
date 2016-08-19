d9af51277786fc20db728498662bf72630c28155
Sbelletier <sbelletier@gmail.com>
2016-08-19 12:36:06 +0200

    L'autoflagellation inclue le .dodg

--- using kivy/FightManager.py
+++ using kivy/FightManager.py
@@ -21,6 +21,20 @@ def attack_melee( atker, defer, nbManeuvers=1, defComp = ".hld" ):
 			diceDmg = randint(1,10)
 			dmg += diceDmg
 		dmg = max(dmg + dmgMod, 0)
+	elif defComp == ".dodg":
+		diceDef, bonusDef = defer.roll_competence(name='.dodg', attrList=['Finesse','Instinct'])
+		scoreDef = diceDef + bonusDef
+		if scoreAtk < scoreDef:
+			dmg = 0
+			dmgMod = 0
+		else:
+			dmgMod = (scoreAtk)/5
+			dmg = 0
+			for i in range(nbManeuvers):
+				diceDmg = randint(1,10)
+				dmg += diceDmg
+			dmg = max(dmg + dmgMod, 0)
+				
 	print 'Defender rolled a ' + str(diceDef) +' for a total of '+str(scoreDef)
 	print 'Damage taken : ' + str(dmg) + ' ( ' + str(dmgMod) + ' due to modifier)'
 
@@ -30,7 +44,9 @@ if __name__ == '__main__':
 	a = Fighter()
 	a.add_competence( Competence('.atk',12,['Physique','Volonte']) )
 	a.comp['.hld'] = Competence('.hld',22,['Stabilite','Volonte'])
-	print "1"
+	print "hold 1"
 	attack_melee(a, a)
-	print "2"
+	print "hold 2"
 	attack_melee(a, a, nbManeuvers=5)
+	print "dodge 1"
+	attack_melee(a, a, defComp=".dodg")
